a2fbd6918e296d43d04c0ad0fd66a820145062ce
ef71a0518ebc075a7f309cca8 <163979047+ef71a0518ebc075a7f309cca8@users.noreply.github.com>
2024-03-21 00:25:59 -0500

    Update HashMaster.py
    redid variable naming
    optimized some functions
    enhanced user friendliness

--- HashMaster.py
+++ HashMaster.py
@@ -1,9 +1,9 @@
 '''
 Made by ef71a0518ebc075a7f309cca8
 https://github.com/ef71a0518ebc075a7f309cca8
-ver 1.0
+ver 1.2
 '''
-import hashlib
+import hashlib as hash_library
 def guide():
     print("\n\nmade by ef71a0518ebc075a7f309cca8")
     print("\nprompt = what is going to be hashed")
@@ -17,100 +17,99 @@ def guide():
     print("example: ")
     print("enter a prompt: mypassword")
     print("enter an algorithm: sha3_512\n\n")
-def hashgen(vari, fileopac):
-    if vari == "Q":
+def generate_text_hash(text_prompt, file_output_active):
+    if text_prompt == "Q":
         raise Exception()
-    func = input("enter an algorithm: ")
+    algorithm = input("enter an algorithm: ")
     try:
-        hashcod = hashlib.new(func)
-        hashcod.update(vari.encode('utf-8'))
-        if fileopac:
-            fileop.write(hashcod.hexdigest() + "  " + vari + "\n")
-        return hashcod.hexdigest()
+        text_hash_operator = hash_library.new(algorithm)
+        text_hash_operator.update(text_prompt.encode('utf-8'))
+        if file_output_active:
+            file_output_object.write(text_hash_operator.hexdigest() + "  " + text_prompt + "\n")
+        return text_hash_operator.hexdigest()
     except:
         try:
-            hashcod.hexdigest(32)
-            digestlen = input("enter a digest length: ")
-            if fileopac:
-                fileop.write(hashcod.hexdigest(int(digestlen)) + "  " + vari + "\n")
-            return hashcod.hexdigest(int(digestlen))
+            text_hash_operator.hexdigest(32)
+            digest_length = input("enter a digest length: ")
+            if file_output_active:
+                file_output_object.write(text_hash_operator.hexdigest(int(digest_length)) + "  " + text_prompt + "\n")
+            return text_hash_operator.hexdigest(int(digest_length))
         except:
             return "failed"
-def filegen(filename, fileopac):
-    if filename == "Q":
+def generate_file_hash(file_input_name, file_output_active):
+    if file_input_name == "Q":
         raise Exception()
-    func = input("enter an algorithm: ")
+    algorithm = input("enter an algorithm: ")
     try:
-        fileopen = open(filename, "rb")
-        fileread = fileopen.read()
-        hashinstance = hashlib.new(func)
-        hashinstance.update(fileread)
-        if fileopac:
-            fileop.write(hashinstance.hexdigest() + "  " + filename + "\n")
-        return hashinstance.hexdigest()
+        file_input_object = open(file_input_name, "rb")
+        file_data = file_input_object.read()
+        file_hash_operator = hash_library.new(algorithm)
+        file_hash_operator.update(file_data)
+        if file_output_active:
+            file_output_object.write(file_hash_operator.hexdigest() + "  " + file_input_name + "\n")
+        return file_hash_operator.hexdigest()
     except:
         try:
-            hashinstance.hexdigest(32)
-            digestlen = input("enter a digest length: ")
-            if fileopac:
-                fileop.write(hashinstance.hexdigest(int(digestlen)) + "  " + filename + "\n")
-            return hashinstance.hexdigest(int(digestlen))
+            file_hash_operator.hexdigest(32)
+            digest_length = input("enter a digest length: ")
+            if file_output_active:
+                file_output_object.write(file_hash_operator.hexdigest(int(digest_length)) + "  " + file_input_name + "\n")
+            return file_hash_operator.hexdigest(int(digest_length))
         except:
             return "failed"
-runtime = True
-while True:
-    fileopactive = False
-    filehashperm = False
-    filehashact = True
-    if input("want a quick guide? y/n: ") == "y":
+execute_loop = True
+while execute_loop:
+    file_output_active = False
+    hash_file_exclusively = False
+    hash_file_active = True
+    if input("want a quick guide? y/n: ") in ["y", "yes"]:
         guide()
-    if input("do you want to output to a file? y/n:") == "y":
-        filepos = input("enter the file path/name: ")
+    if input("do you want to output to a file? y/n:") in ["y", "yes"]:
+        file_output_name = input("enter the file path/name: ")
         try:
-            fileop = open(filepos, "x")
-            fileopactive = True
+            file_output_object = open(file_output_name, "x")
+            file_output_active = True
         except:
             try:
-                switch = input("this file already exists!\n append(a)/overwrite(w): ")
-                if switch == "append" or "a":
-                    fileop = open(filepos, "a")
-                    fileopactive = True
-                if switch == "overwrite" or "w":
-                    fileop = open(filepos, "w")
-                    fileopactive = True
+                output_type_switch = input("this file already exists!\n append(a)/overwrite(w): ")
+                if output_type_switch in ["append", "a"]:
+                    file_output_object = open(file_output_name, "a")
+                    file_output_active = True
+                if output_type_switch in ["overwrite", "w"]:
+                    file_output_object = open(file_output_name, "w")
+                    file_output_active = True
                 else:
                     print("file operation not recognized")
             except:
                 print("file operation failed")
-                fileopactive = False
-    userchoice = input("are you hashing files? only-files(o)/mixed(y)/no-files(n)")
-    if userchoice == "o":
-        filehashperm = True
-        filehashact = False
-    elif userchoice == "n":
-        filehashact = False
-    print(hashlib.algorithms_available)
-    while True:
+                file_output_active = False
+    user_type_input_choice = input("are you hashing files? only-files(o)/mixed(y)/no-files(n)")
+    if user_type_input_choice == "o":
+        hash_file_exclusively = True
+        hash_file_active = False
+    elif user_type_input_choice == "n":
+        hash_file_active = False
+    print("\n" + (", ".join(list(hash_library.algorithms_available))) + "\n")
+    while execute_loop:
         try:
-            if filehashperm:
-                    print(filegen(input("\n(Q to restart)\nenter file path/name: "), fileopactive))
-            elif filehashact:
-                userchoice = input("\n(Q to restart)\nfile(f)/prompt(p): ")
-                if userchoice == "f":
-                        print(filegen(input("\n(Q to restart)\n\nenter file path/name: "), fileopactive))
-                elif userchoice == "p":
-                    print(hashgen(input("\n(Q to restart)\n(Q to quit)\n\nenter a prompt: "), fileopactive))
-                elif userchoice == "Q":
+            if hash_file_exclusively:
+                    print(generate_file_hash(input("\n(Q to restart)\nenter file path/name: "), file_output_active))
+            elif hash_file_active:
+                user_type_input_choice = input("\n(Q to restart)\nfile(f)/prompt(p): ")
+                if user_type_input_choice in ["f", "file"]:
+                        print(generate_file_hash(input("\n(Q to restart)\n\nenter file path/name: "), file_output_active))
+                elif user_type_input_choice in ["p", "prompt"]:
+                    print(generate_text_hash(input("\n(Q to restart)\n(Q to quit)\n\nenter a prompt: "), file_output_active))
+                elif user_type_input_choice in ["Q", "quit"]:
                     raise Exception()
             else:
-                print(hashgen(input("\n(Q to restart)\nenter a prompt: "), fileopactive))
+                print(generate_text_hash(input("\n(Q to restart)\nenter a prompt: "), file_output_active))
         except:
-            if input("\n(all other inputs ignored)\nenter Q again to quit: ") == "Q":
-                runtime = False;
+            if input("\n(all other inputs ignored)\nenter Q again to quit: ") in ["Q", "quit"]:
+                execute_loop = False;
             break
-    if not runtime:
-        break
-    print("\n\n\nresetting instance\n\n\n")
+    if execute_loop:
+        print("\n\n\nresetting instance\n\n\n")
 
 
 
